6dc44bd4056b87f76b1bcdcfe69455f878137255
chaoyue520 <32260272+chaoyue520@users.noreply.github.com>
2017-10-31 12:04:11 +0800

    json形式数据转化成数据框的形式

---
+++ dict_trans_df.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import json
+import pandas as pd
+
+#python读取hive数据
+new_data=os.popen(""" hive -e "use dwi ; select base_info,bank_info,account_info from risk_event where dt='20171029' limit 10;"  """)
+#new_data=os.popen(""" hive -f /home/fsg/jiwenchao/non_white_cluster/xxx.sql  """)
+
+allLines=new_data.readlines()
+new_data.close()
+
+# 初始化一个字典
+key_list=['brAlsM3CellNbankAllnum','unionLoanCode1','app_shopcredit_1_5_count_success_1d']
+dic={key:[] for key in key_list}
+
+
+# 方法1：生成dataframe形式，先生成字典
+for eachline in allLines:
+    a=json.loads(eachline.split('\t')[0])
+    for i in range(len(key_list)):
+        if key_list[i] in a.keys():
+            dic[key_list[i]].append(a[key_list[i]])
+        else :
+            dic[key_list[i]].append("NULL")
+
+# 然后字典转化成数据框
+dic_df=pd.DataFrame.from_dict(dic,orient='columns')
+
+
+# 方法2：文件写到本地，直接存储成数据框的形式
+with open("result.txt","w") as file:
+    for line in allLines:
+        a=json.loads(line.split("\t")[0])
+        for i in range(len(key_list)):
+            if key_list[i] in a.keys():
+                file.write(a[key_list[i]] + "\t")
+            else:
+                file.write("NULL" + "\t")
+        file.write("\n")
+
